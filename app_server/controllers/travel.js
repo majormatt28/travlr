fde3daebf5759d689dd22fcf63cdaf6b039514b8
Matthew <elijah2895@hotmail.com>
2022-05-18 22:35:44 -0400

    module 3 finished

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
 // GET homepage
+let fs = require('fs');
+
+let trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaway' });
+    res.render('travel', { title: 'Travlr Getaway', trips });
 };
 
 module.exports = {
