063666f2926d572eb8b13c96eea216431dee0433
Matthew <elijah2895@hotmail.com>
2022-05-23 09:44:08 -0400

    updated a few things

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
-
+const hbs = require('hbs');
 const app = express();
 
 // view engine setup
